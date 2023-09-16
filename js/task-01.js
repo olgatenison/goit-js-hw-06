0c46339a1b06268407364ff296f267752e1907bf
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-16 21:17:44 +0300

    01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const allCategoriesEl = document.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${allCategoriesEl.length}`);
+
+allCategoriesEl.forEach((e) => {
+  let titleEl = e.firstElementChild.textContent;
+  console.log(`Category: ${titleEl}`);
+  let countSubEl = e.lastElementChild.children.length;
+  console.log(`Elements: ${countSubEl}`);
+});
---
+++ js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingridients = document.querySelector("#ingredients");
