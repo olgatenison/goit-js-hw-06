2b78029621b7253fec264765995de8138a4ced62
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-23 00:05:08 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElement = event.currentTarget.elements;
+  const mail = formElement.email.value;
+  const password = formElement.password.value;
+  const formData = { mail, password };
+
+  if (mail !== "" && password !== "") {
+    console.log(formData);
+  } else {
+    event.currentTarget.reset();
+    return alert("All fields must be filled!");
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const bg = document.querySelector("body");
+const color = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener("click", changeColor){
+  
+};
