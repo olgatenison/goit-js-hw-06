54edf2496a2a7c249a0fdb9a93ed666be4a6fc21
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-24 21:43:36 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,15 +2,18 @@ const bg = document.querySelector("body");
 const color = document.querySelector(".color");
 const btn = document.querySelector(".change-color");
 
+// Add a click handler to the button
+btn.addEventListener("click", changeColor);
 
-
-
+// Get a random hexadecimal color
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-btn.addEventListener("click", changeColor){
-  
-};
+// Click handler for the button
+function changeColor() {
+  const bgColor = getRandomHexColor();
+  bg.style.backgroundColor = `${bgColor}`;
+  color.textContent = `${bgColor}`;
+}
