e4a4c506eb2b8ae9679c715e5b33c2d7ccf31808
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-27 22:37:21 +0300

    7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputSizeControl = document.querySelector("#font-size-control");
 const outputTextSize = document.querySelector("#text");
 
+outputTextSize.style.fontSize = inputSizeControl.value + "px";
+
 inputSizeControl.addEventListener("input", handleFontSizeChange);
 
 function handleFontSizeChange(event) {
